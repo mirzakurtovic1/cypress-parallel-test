d19eb9e23a96d227b1a6ab1344e1865a4413f2df
Mirza Kurtovic <mirza@della.ai>
2023-07-27 14:28:57 +0200

    parallel

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
-
+const cypressSplit = require('cypress-split')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      cypressSplit(on, config)
       // implement node event listeners here
+      return config
     },
   },
 });
